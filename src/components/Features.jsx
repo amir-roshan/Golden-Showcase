fd028210648ad2df42cadbab4916615bc26bc9a1
amir-roshan <amirvalielahi@gmail.com>
2025-02-04 23:48:23 -0800

    Add additional BentoCard components and enhance Features section with new video content

--- src/components/Features.jsx
+++ src/components/Features.jsx
@@ -1,3 +1,5 @@
+import { TiLocationArrow } from "react-icons/ti";
+
 const BentoCard = ({ src, title, description, isComingSoon }) => {
   return (
     <div className="relative size-full">
@@ -14,6 +16,11 @@ const BentoCard = ({ src, title, description, isComingSoon }) => {
           {description && (
             <p className="mt-3 mx-w-64 text-xs md:text-base bento-description">
               {description}
+            </p>
+          )}
+          {isComingSoon && (
+            <p className="px-3 py-2 bg-transparent hover:bg-black rounded-2xl text-gray-300 absolute bottom-0 text-xs md:text-base font-semibold bento-description transition-all duration-300 cursor-pointer">
+              Coming Soon
             </p>
           )}
         </div>
@@ -38,7 +45,7 @@ const Features = () => {
         </div>
         <div className="border-hsla relative mb-7 h-96 w-full overflow-hidden rounded-md md:h-[65vh]">
           <BentoCard
-            src="videos/feature-1.mp4"
+            src="/videos/feature-1.mp4"
             title={
               <>
                 radie<b>n</b>t
@@ -51,6 +58,57 @@ const Features = () => {
                         "
             isComingSoon={true}
           />
+        </div>
+        <div className="grid h-[135vh] grid-cols-2 grid-rows-3 gap-7">
+          <div className="bento-titl_1 row-span-1 md:col-span-1 md:row-span-2">
+            <BentoCard
+              src={"/videos/feature-2.mp4"}
+              title={<>zigma</>}
+              description={
+                "The NFT collection merging Zentry’s IP, AI, and gaming—pushing the boundaries of NFT innovation."
+              }
+              isComingSoon={true}
+            />
+          </div>
+          <div className="bento-tilt_1 row-span-1 ms-32 md:col-span-1 md:ms-0">
+            <BentoCard
+              src="/videos/feature-3.mp4"
+              title={
+                <>
+                  n<b>e</b>xux
+                </>
+              }
+              description="The ultimate NFT marketplace, where you can buy, sell, and trade NFTs from various projects."
+            />
+          </div>
+          <div className="bento-tilt_1 me-14 md:col-span-1 md:me-0">
+            <BentoCard
+              src="/videos/feature-4.mp4"
+              title={
+                <>
+                  az<b>u</b>l
+                </>
+              }
+              description="A cross-world AI agent that helps you navigate the Metagame layer and make the most of your experience."
+            />
+          </div>
+          <div className="bento-tilt_1">
+            <div className="flex size-full flex-col justify-between bg-violet-300 p-5">
+              <h1 className="bento-title special-font max-w-64 text-black">
+                M<b>o</b>re co<b>m</b>ing soon!
+              </h1>
+              <TiLocationArrow className="m-5 scale-[5] self-end" />
+            </div>
+          </div>
+          <div className="bento-tilt_2">
+            <video
+              src="/videos/feature-5.mp4"
+              loop
+              muted
+              autoPlay
+              className="size-full object-cover object-center"
+            ></video>
+          </div>
         </div>
       </div>
     </section>
