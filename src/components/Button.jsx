11e1c660cc55854c0506fd21beb852acbd59637a
amir-roshan <amirvalielahi@gmail.com>
2025-03-01 15:03:26 -0800

    Add interactive features to Story component and implement new Contact and Footer components

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -4,7 +4,7 @@ const Button = ({ title, id, rightIcon, leftIcon, containerClass }) => {
   return (
     <button
       id={id}
-      className={`group relative z-10 w-fit cursor-pointer overflow-hidden rounded-full bg-violet-50 px-7 py-3 text-balance ${containerClass}`}
+      className={`text-black group relative z-10 w-fit cursor-pointer overflow-hidden rounded-full bg-violet-50 px-7 py-3 text-balance ${containerClass}`}
     >
       {leftIcon}
       <span className="relative inline-flex overflow-hidden font-general text-xs uppercase">
--- src/components/Story.jsx
+++ src/components/Story.jsx
@@ -1,9 +1,51 @@
+import gsap from "gsap";
 import { AnimatedTitle } from "./AnimatedTitle";
 import { useRef } from "react";
+import RoundedCorners from "./RoundedCorners";
+import Button from "./Button";
 
 const Story = () => {
   const frameRef = useRef(null);
-  const handleMouseLeve = () => {};
+
+  const handleMouseLeve = () => {
+    const element = frameRef.current;
+    gsap.to(element, {
+      duration: 0.3,
+      rotateX: 0,
+      rotateY: 0,
+      transformPerspective: 500, // perspective for 3d effect
+      ease: "power1.inOut",
+    });
+  };
+
+  const handleMouseMove = (e) => {
+    const { clientX, clientY } = e;
+    const element = frameRef.current;
+
+    if (!element) return;
+
+    const rect = element.getBoundingClientRect();
+    const x = clientX - rect.left; // x position within the element.
+    const y = clientY - rect.top; // y position within the element.
+
+    const centerX = rect.width / 2; // center of the element
+    const centerY = rect.height / 2;
+
+    const rotatetX = ((y - centerY) / centerY) * -10;
+    const rotatetY = ((x - centerY) / centerX) * -10;
+
+    gsap.to(
+      element,
+      {
+        duration: 0.3,
+        rotateX: rotatetX,
+        rotateY: rotatetY,
+        transformPerspective: 500, // perspective for 3d effect
+        ease: "power1.inOut",
+      },
+      { rotationY: rotatetY, rotationX: rotatetX }
+    );
+  };
   return (
     <section id="story" className="min-h-dvh w-screen bg-black text-blue-50">
       <div className="flex size-full flex-col items-center py-10 pb-24">
@@ -21,6 +63,10 @@ const Story = () => {
             <div className="story-img-mask">
               <div className="story-img-content">
                 <img
+                  onMouseLeave={handleMouseLeve}
+                  onMouseUp={handleMouseLeve}
+                  onMouseEnter={handleMouseLeve}
+                  onMouseMove={handleMouseMove}
                   ref={frameRef}
                   src="/img/entrance.webp"
                   alt="entrance"
@@ -28,6 +74,21 @@ const Story = () => {
                 />
               </div>
             </div>
+            <RoundedCorners />
+          </div>
+        </div>
+        <div className="-mt-80 flex w-full justify-center md:-mt-64 md:me-44 md:justify-end">
+          <div className="flex h-full w-fit flex-col items-center md:items-start">
+            <p className="mt-3 max-w-sm text-center font-circular-web text-violet-50 md:text-start">
+              The Open IP Universe The story of a hidden realm Where realms
+              converge, lies Zentry and the boundless pillar. Discover its
+              secrets and shape your fate amidst infinite opportunities.
+            </p>
+            <Button
+              id={"realm-button"}
+              title={"discover prologue"}
+              containerClass={"mt-5"}
+            />
           </div>
         </div>
       </div>
