829ca8568ba77d7b8651cc2c875e933fab5bedf4
amir-roshan <amirvalielahi@gmail.com>
2025-03-01 16:05:24 -0800

    Add Contact and Footer components to enhance app structure and user engagement

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,5 +1,46 @@
+import { FaDiscord } from "react-icons/fa";
+import { FaFacebookF } from "react-icons/fa";
+import { FaInstagram } from "react-icons/fa";
+import { FaGithub } from "react-icons/fa";
+import { FaTwitch } from "react-icons/fa";
+
+const links = [
+  { href: "https://www.facebook.com", icon: <FaFacebookF /> },
+  { href: "https://www.twitch.com", icon: <FaTwitch /> },
+  { href: "https://www.instagram.com", icon: <FaInstagram /> },
+  { href: "https://www.discord.com", icon: <FaDiscord /> },
+  { href: "https://www.github.com", icon: <FaGithub /> },
+];
+
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <footer className="w-screen bg-violet-300 py-4 text-black">
+      <div className="container mx-auto flex flex-col items-center justify-between gap-4 px-4 md:flex-row md:px-10">
+        <p className="text-center text-sm font-light md:text-left">
+          &copy; 2025 Zentry. All rights reserved.
+        </p>
+        <div className="flex justify-center gap-4 items-center">
+          {links.map(({ href, icon }) => (
+            <a
+              target="_blank"
+              rel="noreferrer noopener" // security measure to prevent XSS attacks
+              key={href}
+              href={href}
+              className="text-black transition-colors duration-500 ease-in-out hover:text-white"
+            >
+              {icon}
+            </a>
+          ))}
+        </div>
+        <a
+          href="#privacy-policy"
+          className="text-center text-sm hover:underline md:text-right"
+        >
+          Privacy Policy
+        </a>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
